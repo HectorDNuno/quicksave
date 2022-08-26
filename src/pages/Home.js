b503cff33d5cf128e602301f9df01b3f579a98ac
hector <hectornuno1994@gmail.com>
2022-08-26 14:26:39 -0700

    adds more code to home components and imports navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+
+const authToken = true;
+
+const handleClick = () => {
+  console.log("clicked");
+};
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <h1>Find a Fireteam</h1>
+      <button className="primary-button" onClick={handleClick}>
+        {authToken ? "Sign Out" : "Create Account"}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
