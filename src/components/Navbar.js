c964fea9581eb692eae68281079a1f200b96c85e
hector <hectornuno1994@gmail.com>
2022-08-31 13:26:39 -0700

    receives setIsSignUp as props, adds classname title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import arrows from "../components/images/rotate-solid.svg";
 
-const Navbar = ({ authToken, setShowModal, showModal }) => {
+const Navbar = ({ authToken, setShowModal, showModal, setIsSignUp }) => {
   const handleClick = () => {
     setShowModal(true);
+    setIsSignUp(false);
   };
 
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={arrows} alt="" />
-        <h1> Quicksave </h1>
+        <h1 className="title"> Quicksave </h1>
       </div>
 
       {!authToken && (
