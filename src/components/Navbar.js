5a3ce77669a10c1dcef52015629f95f65acb3c52
hector <hectornuno1994@gmail.com>
2022-08-31 12:28:47 -0700

    receieves prop from home component, adds onClick and handleClick events

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 import arrows from "../components/images/rotate-solid.svg";
 
-const Navbar = ({ authToken }) => {
+const Navbar = ({ authToken, setShowModal, showModal }) => {
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
   return (
     <nav>
       <div className="logo-container">
@@ -9,7 +13,11 @@ const Navbar = ({ authToken }) => {
         <h1> Quicksave </h1>
       </div>
 
-      {!authToken && <button className="nav-button"> Log in </button>}
+      {!authToken && (
+        <button className="nav-button" onClick={handleClick} disabled={showModal}>
+          Log in
+        </button>
+      )}
     </nav>
   );
 };
