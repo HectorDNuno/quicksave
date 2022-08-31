5ad26c45fbc0f7fb880b6ee953f691ee0475db5f
hector <hectornuno1994@gmail.com>
2022-08-31 12:27:58 -0700

    sets showModal state, passes props to navbar, adds conditional for modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-
-const authToken = false;
-
-const handleClick = () => {
-  console.log("clicked");
-};
+import AuthModal from "../components/AuthModal";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const authToken = false;
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="overlay">
-      <Navbar authToken={authToken} />
+      <Navbar authToken={authToken} setShowModal={setShowModal} showModal={showModal} />
       <h1>Find a Fireteam</h1>
       <button className="primary-button" onClick={handleClick}>
         {authToken ? "Sign Out" : "Create Account"}
       </button>
+
+      {showModal && <AuthModal setShowModal={setShowModal} />}
     </div>
   );
 };
