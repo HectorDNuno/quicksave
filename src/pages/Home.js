8f2b0e243fddecffa0c45d068a814fadb14b4b05
hector <hectornuno1994@gmail.com>
2022-08-31 13:24:21 -0700

    sets state of isSignUp and passes it to navbar and authmodal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,24 @@ import AuthModal from "../components/AuthModal";
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
 
   const authToken = false;
 
   const handleClick = () => {
     setShowModal(true);
+    setIsSignUp(true);
   };
 
   return (
     <div className="overlay">
-      <Navbar authToken={authToken} setShowModal={setShowModal} showModal={showModal} />
+      <Navbar authToken={authToken} setShowModal={setShowModal} showModal={showModal} setIsSignUp={setIsSignUp} />
       <h1>Find a Fireteam</h1>
       <button className="primary-button" onClick={handleClick}>
         {authToken ? "Sign Out" : "Create Account"}
       </button>
 
-      {showModal && <AuthModal setShowModal={setShowModal} />}
+      {showModal && <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />}
     </div>
   );
 };
