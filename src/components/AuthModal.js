7176503631c9148933f998748a44611ac79c753c
hector <hectornuno1994@gmail.com>
2022-08-31 12:31:39 -0700

    receieves props from navbar, creates event and event handler

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const AuthModal = () => {
-  return <div>AuthModal</div>;
+const AuthModal = ({ setShowModal }) => {
+  const handleClick = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="auth-modal">
+      <div onClick={handleClick}>x</div>
+      AuthModal
+    </div>
+  );
 };
 
 export default AuthModal;
