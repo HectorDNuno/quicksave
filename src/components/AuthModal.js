7c410338ea6408f5bd0eab4388d9990efac5854b
hector <hectornuno1994@gmail.com>
2022-08-31 13:08:10 -0700

    sets state and creates forms for email, password, and password confirmation, creates event handler for onSubmit event, adds line at the bottom of form with text that says get the app

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,14 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [errors, setErrors] = useState(null);
+
   const handleClick = () => {
     setShowModal(false);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      if (isSignUp && password !== confirmPassword) {
+        setErrors("passwords need to match!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const isSignUp = true;
+
   return (
     <div className="auth-modal">
-      <div onClick={handleClick}>x</div>
-      AuthModal
+      <div className="close-icon" onClick={handleClick}>
+        x
+      </div>
+      <h2>{isSignUp ? "Create Account" : "Login"}</h2>
+      <p>By clicking Login you are agreeing to our terms and conditions</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSignUp && (
+          <input
+            type="password"
+            id="password-check"
+            name="password-check"
+            placeholder="confirm password"
+            required={true}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+
+        <input className="secondary-button" type="submit" />
+        <p>{errors}</p>
+      </form>
+      <hr />
+      <h2>GET THE APP</h2>
     </div>
   );
 };
