d9c7d3aa45dc9a97a8c112b51c94a0a35c50af60
hector <hectornuno1994@gmail.com>
2022-09-05 15:32:11 -0700

    adds form for gender

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -61,6 +61,95 @@ const Onboarding = () => {
                 onChange={handleChange}
               />
             </div>
+
+            <label>Gender</label>
+            <div className="multiple-input-container">
+              <input
+                id="man-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="man"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="man-gender-identity">Man</label>
+
+              <input
+                id="woman-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="woman"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="woman-gender-identity">Woman</label>
+
+              <input
+                id="non-binary-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="non-binary"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="non-binary-gender-identity">Non-binary</label>
+
+              <input
+                id="other-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="other"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="other-gender-identity">Other</label>
+            </div>
+
+            <label htmlFor="show-gender">Show gender on my profile</label>
+            <input id="show-gender" type="checkbox" name="show-gender" onChange={handleChange} checked={false} />
+
+            <label>Show Me</label>
+            <div className="multiple-input-container">
+              <input
+                id="man-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="man"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="man-gender-interest">Man</label>
+
+              <input
+                id="woman-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="woman"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="woman-gender-interest">Woman</label>
+
+              <input
+                id="non-binary-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="non-binary"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="non-binary-gender-interest">Non-binary</label>
+
+              <input
+                id="everyone-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="everyone"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="everyone-gender-interest">Everyone</label>
+            </div>
           </section>
         </form>
       </div>
