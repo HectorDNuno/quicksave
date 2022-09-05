2c8bca15bdd282f021ec94591397b67e41a55dbc
hector <hectornuno1994@gmail.com>
2022-09-05 14:14:27 -0700

    adds conditional rendering that changes appearance of logo depending on minimal value

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import arrows from "../components/images/rotate-solid.svg";
+import blackArrows from "../components/images/rotate-solid-black.svg";
 
-const Navbar = ({ authToken, setShowModal, showModal, setIsSignUp }) => {
+const Navbar = ({ authToken, setShowModal, showModal, setIsSignUp, minimal }) => {
   const handleClick = () => {
     setShowModal(true);
     setIsSignUp(false);
@@ -10,11 +11,11 @@ const Navbar = ({ authToken, setShowModal, showModal, setIsSignUp }) => {
   return (
     <nav>
       <div className="logo-container">
-        <img className="logo" src={arrows} alt="" />
-        <div className="title"> quicksave </div>
+        <img className="logo" src={minimal ? blackArrows : arrows} alt="" />
+        <div className={minimal ? "dark-title" : "title"}> quicksave </div>
       </div>
 
-      {!authToken && (
+      {!authToken && !minimal && (
         <button className="nav-button" onClick={handleClick} disabled={showModal}>
           Log in
         </button>
