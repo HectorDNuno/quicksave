06cc0a73f5cdec12acf1d9ea3e741128a38a27e2
hector <hectornuno1994@gmail.com>
2022-09-05 15:30:53 -0700

    adds form for name and birthday

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,7 +1,71 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
+  const handleChange = () => {
+    console.log("changed");
+  };
+
+  return (
+    <>
+      <Navbar minimal={true} setShowModal={() => {}} showModal={false} />
+      <div className="onboarding">
+        <h2>Create Account</h2>
+
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label htmlFor="first_name">First Name</label>
+            <input
+              id="first_name"
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              required={true}
+              value={""}
+              onChange={handleChange}
+            />
+
+            <label>Birthday</label>
+            <div className="multiple-input-container">
+              <input
+                id="dob_day"
+                type="number"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+
+              <input
+                id="dob_month"
+                type="number"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+
+              <input
+                id="dob_year"
+                type="number"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+            </div>
+          </section>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Onboarding;
