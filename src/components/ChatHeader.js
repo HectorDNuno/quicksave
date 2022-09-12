2297fc50b43267434ef93785d6afbd8493a18252
hector <hectornuno1994@gmail.com>
2022-09-12 16:53:33 -0700

    adds starter code for component

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ChatHeader = () => {
-  return <div>ChatHeader</div>;
+  return (
+    <div className="chat-container-header">
+      <div className="profile">
+        <div className="img-container">
+          <img src="" alt="" />
+        </div>
+        <h3>Username</h3>
+      </div>
+      <i className="log-out-icon">←</i>
+    </div>
+  );
 };
 
 export default ChatHeader;
