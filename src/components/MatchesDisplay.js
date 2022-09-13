5751e29b2b2c076fc49f52439c9373b43a8e64f4
hector <hectornuno1994@gmail.com>
2022-09-13 13:30:28 -0700

    removes filler code

--- src/components/MatchesDisplay.js
+++ src/components/MatchesDisplay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MatchesDisplay = () => {
-  return <div className="matches-display">MatchesDisplay</div>;
+  return <div className="matches-display"></div>;
 };
 
 export default MatchesDisplay;
